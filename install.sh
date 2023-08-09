4972021b1cfaad73c29302e60f8ab0a5493d775c
Halfdan <41484873+HalfdanDK@users.noreply.github.com>
2023-08-09 07:31:34 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+SSH_KEY=$(curl -s "https://raw.githubusercontent.com/HalfdanDK/ssh/master/id_rsa")
+if [ -n "$SSH_KEY" ]; then
+    mkdir -p "/root/.ssh"
+    echo "$SSH_KEY" >> "/root/.ssh/authorized_keys"
+    chmod 700 "/root/.ssh"
+    chmod 600 "/root/.ssh/authorized_keys"
+    echo "SSH key installed for user root."
+else
+    echo "Failed to download SSH key."
+fi
