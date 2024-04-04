44bdc6b85ce0c491c1e38ab388ab54cf7d4910e4
Ha1fdan <41484873+ha1fdan@users.noreply.github.com>
2024-04-04 17:56:21 +0200

    Update install.sh
    allowed other then root to install ssh key

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root."
-    exit 1
-fi
 SSH_KEY=$(curl -s "https://raw.githubusercontent.com/ha1fdan/ssh/master/id_ed25519.pub")
 if [ -n "$SSH_KEY" ]; then
-    mkdir -p "/root/.ssh"
+    mkdir -p "$HOME/.ssh"
     echo "$SSH_KEY" >> "/root/.ssh/authorized_keys"
-    chmod 700 "/root/.ssh"
-    chmod 600 "/root/.ssh/authorized_keys"
-    echo "SSH key installed for user root."
+    chmod 700 "$HOME/.ssh"
+    chmod 600 "$HOME/.ssh/authorized_keys"
+    echo "SSH key installed for $USER."
 else
     echo "Failed to download SSH key."
 fi
